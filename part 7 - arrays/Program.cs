431fd3830e90786ff0c5b21264b3fbf65f3342f3
Tilson Beattie <tilsonbeats@gmail.com>
2020-10-12 17:40:46 -0400

    Add project files.

---
+++ part 7 - arrays/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace part_7___arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            
+            
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+
+    }
+}
